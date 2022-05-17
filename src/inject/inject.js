2eb1e445f9354e124d36c5f721634403130374ef
sk.abdul haq <75989562+skabdulhaq@users.noreply.github.com>
2022-05-17 22:59:41 +0530

    all bugs fixed

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -37,30 +37,14 @@ setInterval(function () {
 		remove_rec();
 	}
 	else if (window.location.href === "https://www.youtube.com/") {
+		document.querySelector("#primary").innerHTML = "<h1 style='color: white; text-align: center;font-size:8rem;align-self: center;;'>Let's Do Something Productive!!</h1>";
+		removeSideNav();
 		if (document.querySelector("#primary > ytd-rich-grid-renderer")) {
 			document.querySelector("#primary > ytd-rich-grid-renderer").remove();
-		}
-		removeSideNav()
-		if (document.querySelector("#guide-inner-content")) {
-			document.querySelector("#guide-inner-content").remove();
-		}
-		if (document.querySelector("#guide-button")) {
-			document.querySelector("#guide-button").click()
-			document.querySelector("#guide-button").remove();
-		}
-		if (document.querySelector("#guide-content")) {
-			document.querySelector("#guide-content").remove();
-		}
-		if (document.querySelector("#content > ytd-mini-guide-renderer")) {
-			document.querySelector("#content > ytd-mini-guide-renderer").remove();
-		}
-		if (document.querySelector("#sections")) {
-			document.querySelector("#sections").remove();
 		}
 		if (window.location.href.includes("www.youtube.com/watch")) {
 			document.querySelector("#movie_player > div.html5-endscreen.ytp-player-content.videowall-endscreen.ytp-show-tiles").remove();
 		}
-		document.querySelector("#primary").innerHTML = "<h1 style='color: white; text-align: center;font-size:8rem;align-self: center;;'>Let's Do Something Productive!!</h1>";
 	}
 	else {
 		removeSideNav();
