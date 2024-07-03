daab808aaffbb69e0fe4f0ac9f36f85e9835a51d
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2024-07-04 04:56:09 +0530

    Update publish.py

--- publish.py
+++ publish.py
@@ -142,7 +142,8 @@ file_upload_status = get_review_status(login_token, operation_id)
 
 while file_upload_status["message"] is None:
     file_upload_status = get_review_status(login_token, operation_id)
-    print(file_upload_status["message"])
+    if file_upload_status["message"]:
+        print(file_upload_status["message"])
 
 if "errors" in list(file_upload_status.keys()) or "errorCode" in list(file_upload_status.keys()):
     if file_upload_status['errors'] or file_upload_status['errorCode']:
