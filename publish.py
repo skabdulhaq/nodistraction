3808ce7c3240e2cbf3d0d9e97c21760fd205a78b
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2024-07-04 05:11:03 +0530

    Update publish.py

--- publish.py
+++ publish.py
@@ -124,7 +124,8 @@ def get_submission_details(token, operation_id):
     if response.status_code == 200:
         return response.json()
     else:
-        logging.error(f"Get submission details failed: {response.status_code} - {response.text}")
+        time.sleep(10)
+        print(f"Get submission details failed: {response.status_code} - {response.text}")
     return None 
 
 zip_folder("./dist", src_folder)
